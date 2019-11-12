ca41306b747bcaf5fb3d775ce7a5a782cdd14093
18chowdhary <schowdhary@olin.edu>
2019-11-12 11:09:29 -0500

    Move files and create dummy translator node

---
+++ inkscape_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/shreya/POE_Inkscape/inkscape_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/shreya/POE_Inkscape/inkscape_ws/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/opt/ros/melodic/bin:/home/shreya/inkscape/bin:/home/shreya/anaconda3/condabin:/home/shreya/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+export PWD="/home/shreya/POE_Inkscape/inkscape_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/shreya/POE_Inkscape/inkscape_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/shreya/POE_Inkscape/inkscape_ws/src:$ROS_PACKAGE_PATH"
---
+++ inkscape_ws/build/test_foo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/shreya/POE_Inkscape/inkscape_ws/src/test_foo
+# Build directory: /home/shreya/POE_Inkscape/inkscape_ws/build/test_foo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
