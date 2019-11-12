7b7252833f4c032d59892ced651a5d69072e8035
Shashank Swaminathan <sh.swami235@gmail.com>
2019-11-12 16:10:00 +0000

    Committing speed_pub

--- sprint2/basic_ros/basic_ros.ino
+++ sprint2/basic_ros/basic_ros.ino
@@ -1,6 +1,5 @@
 #include <ros.h>
-#include <std_msgs/Int8.h>
-#include <std_msgs/String.h>
+#include <geometry_msgs/Twist.h>
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
@@ -10,19 +9,24 @@ Adafruit_DCMotor *leftMotor = AFMS.getMotor(1); // Left Motor
 ros::NodeHandle  nh;
 
 // initialize variable for the motor speed
-int Mspeed = 30;
+int V = 30;
+int w = 1;
 int temp = 0;
 char info[16] = "";
 
-void messageCb( const std_msgs::Int8& mspeed){
-  temp = mspeed.data;
-  itoa(temp, info, 10);
-  nh.loginfo(info);
-  Mspeed = temp;
+void ctrlsCb( const geometry_msgs::Twist& mspeed){
+  V = mspeed.linear.x;
+  w = mspeed.angular.z;
 }
 
-ros::Subscriber<std_msgs::Int8> sub("/motor_speed", &messageCb );
-std_msgs::String recv_msg;
+void ctrlsCb( const geometry_msgs::Twist& mspeed){
+  V = mspeed.linear.x;
+  w = mspeed.angular.z;
+}
+
+ros::Subscriber<geometry_msgs::Twist> sub("/motor_ctrls/robot1", &ctrlsCb );
+ros::Subscriber<geometry_msgs::Twist> sub("/motor_ctrls/robot1", &flagsCb );
+geometry_msgs::Twist recv_msg;
 ros::Publisher chatter("/recv_m_speed", &recv_msg);
 
 void setup()
@@ -35,38 +39,12 @@ void setup()
 
 void loop()
 {
-  setMotorSpeed(Mspeed, Mspeed);
-  forwardMotion();
-  recv_msg.data = info;
+  recv_msg.linear.x = V;
+  recv_msg.angular.z = w;
   chatter.publish( &recv_msg );
+  temp = V;
+  itoa(temp, info, 10);
+  nh.loginfo(info);
   nh.spinOnce();
   delay(50);
-}
-
-/// set motor speeds with the parameter
-void setMotorSpeed(int RMspeed, int LMspeed) {
-  rightMotor->setSpeed(RMspeed+3);
-  leftMotor->setSpeed(LMspeed);
-}
-
-void forwardMotion() {
-  rightMotor->run(FORWARD);
-  leftMotor->run(FORWARD);
-}
-
-void backwardMotion() {
-  rightMotor->run(BACKWARD);
-  leftMotor->run(BACKWARD);
-}
-
-void turnRight(int t) {
-  rightMotor->run(BACKWARD);
-  leftMotor->run(FORWARD);
-  delay(t);
-}
-
-void turnLeft(int t){
-  rightMotor->run(FORWARD);
-  leftMotor->run(BACKWARD);
-  delay(t);
 }
